cf6215f7a170f2b5ed4ba503563294d8aa62bfe9
ChernovGV <gc9234928530@gmail.com>
2022-06-15 20:57:54 +0700

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,55 @@
 package main
 
-type Environment map[string]EnvValue
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
-type EnvValue struct {
-	Value      string
-	NeedRemove bool
-}
+type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	environments := make(Environment)
+
+	for _, f := range files {
+		filePath := path.Join(dir, f.Name())
+		file, err := os.Open(filePath)
+		if err != nil {
+			return nil, err
+		}
+
+		firstLine := getFirstLineFromFile(file)
+		environments[f.Name()] = clearLine(firstLine)
+
+		file.Close()
+	}
+
+	return environments, nil
+}
+
+func getFirstLineFromFile(file *os.File) string {
+	scanner := bufio.NewScanner(file)
+	var firstLine string
+	if scanner.Scan() {
+		return scanner.Text()
+	}
+
+	return firstLine
+}
+
+func clearLine(firstLine string) string {
+	firstLine = strings.TrimRight(firstLine, " \t\n")
+	firstLine = string(bytes.ReplaceAll([]byte(firstLine), []byte("\x00"), []byte("\n")))
+
+	return firstLine
 }
